de237c5756873905136ed2ddcd422bc324325050
Adrian Lacheta <adiczq@gmail.com>
2023-09-11 11:36:47 +0200

    initial commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,105 @@
-// const fs = require('fs/promises')
+import * as path from "path";
+import { readFile, writeFile } from "fs/promises";
+import { nanoid } from "nanoid";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const getContacts = async () => {
+  try {
+    const contactsJson = await readFile(contactsPath);
+    const contacts = JSON.parse(contactsJson);
+    return contacts;
+  } catch (err) {
+    console.error("Error reading contacts from file:", err);
+    throw err;
+  }
+};
 
-const removeContact = async (contactId) => {}
+const setContacts = async (data) => {
+  try {
+    await writeFile(contactsPath, JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error("Error writinf contacts to file: ", err);
+    throw err;
+  }
+};
 
-const addContact = async (body) => {}
+export const listContacts = async () => {
+  try {
+    return await getContacts();
+  } catch (err) {
+    console.error("Error getting contacts list: ", err);
+    throw err;
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+export const getContactById = async (contactId) => {
+  try {
+    const contacts = await getContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact;
+  } catch (err) {
+    console.error(`Error getting contact with id: ${contactId}`, err);
+    throw err;
+  }
+};
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+export const removeContact = async (contactId) => {
+  try {
+    const contacts = await getContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (!contact) {
+      console.log(`COntact with id: ${contactId} notfound`);
+      return false;
+    }
+    const newList = contacts.filter((contact) => contact.id !== contactId);
+    await setContacts(newList);
+    console.log(`Contact removed`);
+    return true;
+  } catch (err) {
+    console.error(`Error removing contact with id: ${contactId}`, err);
+    throw err;
+  }
+};
+
+export const addContact = async (body) => {
+  try {
+    const contacts = await getContacts();
+    const { name, email, phone } = body;
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await setContacts(contacts);
+    console.log(`Contact added`);
+    return newContact;
+  } catch (err) {
+    console.error(`Error adding contact `, err);
+    throw err;
+  }
+};
+
+export const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await getContacts();
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
+    if (contactIndex === -1) {
+      console.log(`Ther's no contact`);
+      return false;
+    }
+    const contact = contacts[contactIndex];
+    const updateContact = { ...contact, ...body };
+    contacts[contactIndex] = updateContact;
+    await setContacts(contacts);
+    console.log(`Contact updated`);
+    return updateContact;
+  } catch (err) {
+    console.error(`Error updating contact `, err);
+    throw err;
+  }
+};
