f53fb4bc0bba6d9c5b3323de5cfc6a9d69ff334c
Adrian Lacheta <adiczq@gmail.com>
2023-09-17 14:17:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 import { app } from "./app.js";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+config();
+
+const uri = process.env.DB_URL;
+
+const startServer = async () => {
+  try {
+    const connection = await mongoose.connect(uri);
+    console.log("Database connection successfull");
+
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (err) {
+    console.error("Connection failed");
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
