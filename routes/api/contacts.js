a35804f75ef078eef3c12c92ee27f0c297a7ab7c
Adrian Lacheta <adiczq@gmail.com>
2023-09-19 21:30:40 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import {
   listContacts,
   getContactById,
@@ -8,29 +7,41 @@ import {
   updateContact,
   updatedStatusContact,
 } from "./../../models/contacts.js";
+import { auth } from "../../config/config-pasport.js";
 
-export const router = express.Router();
+export const contactsRouter = express.Router();
 
-router.get("/", async (req, res, next) => {
+const pagination = (array, page, limit) => {
+  const startIndex = (page - 1) * limit;
+  return array.slice(startIndex, startIndex + limit);
+};
+
+contactsRouter.get("/", auth, async (req, res, next) => {
   try {
-    const contacts = await listContacts();
-
-    return res.json({
+    const { id: userID } = req.user;
+    const contacts = await listContacts(userID);
+    const { page = 1, limit = 20, favorite } = req.query;
+    let filterContact = contacts;
+    if (favorite === "true") {
+      filterContact = contacts.filter((contact) => contact.favorite);
+    }
+    const paginatedContacts = pagination(filterContact, page, limit);
+    return res.status(200).json({
       status: "success",
       code: 200,
-      data: { contacts },
+      data: { contacts: paginatedContacts },
     });
   } catch (err) {
     res.status(500).json(`Error getting the contact list : ${err}`);
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  const { id } = req.params;
+contactsRouter.get("/:id", auth, async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { id: contactId } = req.params;
   try {
-    const contact = await getContactById(id);
-
-    return res.json({
+    const contact = await getContactById(userId, contactId);
+    return res.status(200).json({
       status: "success",
       code: 200,
       data: { contact },
@@ -40,31 +51,29 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
-  const { body } = req;
-
+contactsRouter.post("/", auth, async (req, res, next) => {
+  const body = req.body;
+  const { id: userId } = req.user;
   if (Object.keys(body).length === 0) {
     return res.status(400).json("Error, empty request is not allowed");
   }
-
   try {
-    const contact = await addContact(body);
-
+    const contact = await addContact(body, userId);
     return res.status(201).json({
       status: "success",
       code: 201,
       data: { contact },
     });
   } catch (err) {
-    res.status(500).json(`An error adding the contact: ${err}`);
+    res.status(500).json(`Error adding the contact: ${err}`);
   }
 });
 
-router.delete("/:id", async (req, res, next) => {
-  const { id } = req.params;
+contactsRouter.delete("/:id", auth, async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { id: contactId } = req.params;
   try {
-    const isContactRemoved = await removeContact(id);
-
+    await removeContact(contactId, userId);
     return res.status(200).json({
       message: `Contact with ID ${id} removed.`,
     });
@@ -73,17 +82,15 @@ router.delete("/:id", async (req, res, next) => {
   }
 });
 
-router.put("/:id", async (req, res, next) => {
-  const { id } = req.params;
+contactsRouter.put("/:id", auth, async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { id: contactId } = req.params;
   const { body } = req;
-
   if (Object.keys(body).length === 0) {
     return res.status(400).json("Error, empty request is not allowed");
   }
-
   try {
-    const updatedContact = await updateContact(id, body);
-
+    const updatedContact = await updateContact(contactId, body, userId);
     return res.json({
       status: "success",
       code: 200,
@@ -94,15 +101,20 @@ router.put("/:id", async (req, res, next) => {
   }
 });
 
-router.patch("/:id", async (req, res, netxt) => {
-  const { id } = req.params;
+contactsRouter.patch("/:id", auth, async (req, res, netxt) => {
+  const { id: userId } = req.user;
+  const { id: contactId } = req.params;
   const { body } = req;
   const { favorite } = body;
   if (!("favorite" in body) || Object.keys(body).length === 0) {
     return res.status(400).json("missing field favorite");
   }
   try {
-    const updateStatus = await updatedStatusContact(id, favorite);
+    const updateStatus = await updatedStatusContact(
+      contactId,
+      favorite,
+      userId
+    );
     return res.json({
       status: "succes",
       code: 200,
