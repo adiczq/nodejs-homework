b5c65ca3f310bb6b64218d46233e43fcb66990a4
Adrian Lacheta <adiczq@gmail.com>
2023-09-19 21:30:48 +0200

    Create users.js

---
+++ routes/api/users.js
@@ -0,0 +1,128 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { getUser, addUser, loginUser, patchUser } from "../../models/users.js";
+import { auth } from "../../config/config-pasport.js";
+
+dotenv.config();
+const secret = process.env.JWT_SECRET;
+export const usersRouter = express.Router();
+
+usersRouter.get("/current", auth, async (req, res, next) => {
+  const { id: userId } = req.user;
+  try {
+    const user = await getUser(userId);
+    if (!user) {
+      return res.status(404).json("user not found");
+    }
+    const { email, subscription } = user;
+    return res.status(200).json({
+      status: "success",
+      code: 200,
+      data: { email, subscription },
+    });
+  } catch (err) {
+    res.status(500).json(`Error getting user ${err}`);
+  }
+});
+
+usersRouter.post("/signup", auth, async (req, res, next) => {
+  const { body } = req;
+  if (Object.keys(body).length === 0) {
+    return res.status(400).json("Error, empty request is not allowed");
+  }
+  try {
+    const user = await addUser(body);
+    if (user === 409) {
+      return res.status(409).json({ message: "Email in use" });
+    }
+    const { email, subscription } = user;
+    return res.status(201).json({
+      status: "success",
+      code: 201,
+      user: { email, subscription },
+    });
+  } catch (err) {
+    res.status(500).json(`Error adding the user: ${err}`);
+  }
+});
+
+usersRouter.post("/login", auth, async (req, res, next) => {
+  const { body } = req;
+  if (Object.keys(body).length === 0) {
+    return res.status(400).json("Error, empty request is not allowed");
+  }
+  try {
+    const user = await loginUser(body);
+    if (!user) {
+      return res.status(400).json(`Email or password is wrong`);
+    }
+    const payload = {
+      id: user.id,
+      username: user.email,
+    };
+    const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+    user.token = token;
+    await user.save();
+    const { email, subscription } = user;
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      token: token,
+      user: { email, subscription },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Error login",
+    });
+  }
+});
+
+usersRouter.post("/logut", auth, async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const user = await getUser(userId);
+    if (!user) {
+      return res.status(401).json({
+        status: "error",
+        code: 401,
+        message: "Not authorized3",
+      });
+    }
+    user.token = null;
+    await user.save();
+    res.status(204).json();
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Error logout",
+    });
+  }
+});
+
+usersRouter.patch("/", auth, async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { body } = req;
+  const { subscription } = body;
+
+  if (!("subscription" in body) || Object.keys(body).length === 0) {
+    return res.status(400).json("Error value subscription!");
+  }
+
+  try {
+    const updatedStatus = await patchUser(subscription, userId);
+    if (updatedStatus === 400) {
+      return res.status(400).json("invalid subscription type");
+    }
+    return res.json({
+      status: "success",
+      code: 200,
+      data: { updatedStatus },
+    });
+  } catch (err) {
+    res.status(500).json(`Error updating : ${err}`);
+  }
+});
